bccc86de1829634b1fe94db7378dc2c484da65db
John Watson <john.watson@gmx.co.uk>
2017-03-21 15:26:30 +0000

    Upgraded ABC example to new API

--- examples/abc/src/Main.purs
+++ examples/abc/src/Main.purs
@@ -3,10 +3,9 @@ module Main where
 import VexTab.Score
 import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Console (CONSOLE, log, logShow)
-import Data.Either (Either(..))
-import Prelude (Unit, bind, (<>))
-import VexTab.Abc.Canonical (toScoreText)
-import VexTab.Abc.Translate (translateText)
+import Prelude (Unit, bind, (<>), ($))
+import VexTab.Abc.Score (renderParsedAbc)
+import Data.Abc.Parser (parse)
 
 config :: Config
 config =
@@ -32,22 +31,11 @@ sampleAbc =
   <> "|: e2e2 e2de | f2ed B3c | d3c d2cd | e3d cdBc |\r\n"
   <> "A2a2 a2gf | e2f2 e3d | cedc BdcB |1 A4 A>AA>B :|2 A4 A4 |\r\n"
 
-vexText :: String
-vexText =
-  let
-    eitherText =
-      translateText sampleAbc
-  in
-    case eitherText of
-      Left err -> err
-      Right score -> toScoreText score
-
-
 main :: forall e. Eff (vt :: VEXTAB, console :: CONSOLE | e) Unit
 main = do
   initialised <- initialise config
   log "initialised?"
   logShow initialised
   log "rendered?"
-  rendered <- render vexText
+  rendered <- renderParsedAbc $ parse sampleAbc
   logShow rendered
